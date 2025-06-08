d2b1e5c681ca4c3529e730cd89719e87f410a5de
Divya Hari Kumar <77283719+divziv@users.noreply.github.com>
2025-06-08 19:04:29 +0530

    Update Find-the-Longest-Word-in-a-String.js
    Adding a C++ solution using a user-defined function and string stream.

--- code-challenges/Week_9/Find-the-Longest-Word-in-a-String.js
+++ code-challenges/Week_9/Find-the-Longest-Word-in-a-String.js
@@ -8,3 +8,40 @@ findLongestWordLength("May the force be with you") ---> 5
 findLongestWordLength("What if we try a super-long word such as otorhinolaryngology") ---> 19
 
 */
+
+/*C++ Solution*/
+
+#include <stdio.h>
+#include <sstream>
+#include <string.h>
+using namespace std;
+
+int findLongestWordLength(const string& sentence) 
+{
+    istringstream ss(sentence);
+    string word;
+    int maxLength = 0;
+
+    while(iss >> word) 
+    {
+        if(word.length() > maxLength) 
+        {
+            maxLength = word.length();
+        }
+    }
+
+    return maxLength;
+}
+
+int main()
+{
+  string input;
+  
+  cout << "Enter a sentence:\n";
+  getline(cin, input);
+  
+  int longest = findLongestWordLength(input);
+  cout << "Length of the longest word: " << longest << "\n";
+    
+  return 0;
+}
